1ded09d18057a13f11f66480f997ee320831d302
aabalalonso <aabalalonso@atanasoff15.danielcastelao.org>
2015-10-01 09:06:34 +0200

    primer commit actividad sistemas informaticos

---
+++ sistemas informaticos actividad/src/sistemas/informaticos/actividad/SistemasInformaticosActividad.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemas.informaticos.actividad;
+
+/**
+ *
+ * @author aabalalonso
+ */
+public class SistemasInformaticosActividad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Actividad sistemas");
+        // TODO code application logic here
+    String cosa="Añadir";
+//    el historial es solo para comparar , nunca para añadir 
+    short variable=3;
+    short variable2=-4;
+    int resultado = variable + variable2;
+    System.out.println("el resultado de la suma es :"+resultado);
+    }
+    
+}
